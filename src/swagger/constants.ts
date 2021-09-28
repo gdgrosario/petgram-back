9e7cb95abd0c7cb62649036a44b83a2177e929d9
iBelando <ignaciobelando@gmail.com>
2021-09-28 00:00:49 -0300

    Swagger Implementation

---
+++ src/swagger/constants.ts
@@ -0,0 +1,4 @@
+export const SWAGGER_API_ROOT = "api/docs";
+export const SWAGGER_API_NAME = "Petgram API";
+export const SWAGGER_API_DESCRIPTION = "API developed by GDGRosario";
+export const SWAGGER_API_CURRENT_VERSION = "1.0";
