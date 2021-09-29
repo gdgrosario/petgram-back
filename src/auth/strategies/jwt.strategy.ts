7bab9237f7b5a047ae80c0849ac32809ddd477e2
KenethSandoval <kenethra.74@gmail.com>
2021-09-28 21:32:47 -0600

    hotfix: jwt strategy validate

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, Inject } from "@nestjs/common";
+import { ConfigType } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+
+import { Strategy, ExtractJwt } from "passport-jwt";
+import config from "../../config/config";
+
+//TODO: mover al package model
+interface IPayloadToken {
+  role: string;
+  sub: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
+  constructor(@Inject(config.KEY) configService: ConfigType<typeof config>) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+      ignoreExpiration: false,
+      secretOrKey: configService.jwtSecret
+    });
+  }
+
+  validate(payload: IPayloadToken) {
+    return payload;
+  }
+}
