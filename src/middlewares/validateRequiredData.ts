6507bae327c9b23810b36249d41a47a79533cb13
Oliver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2022-04-09 04:34:12 -0500

    Modify error submission

--- src/middlewares/validateRequiredData.ts
+++ src/middlewares/validateRequiredData.ts
@@ -3,7 +3,8 @@ import { Request, Response, NextFunction } from 'express';
 export const validateRequiredData = (req: Request, res: Response, next: NextFunction) => {
   //saber que ruta esta llamando
   const currentRoute = req.path
-
+  console.log(currentRoute)
+  console.log(req.body)
   // campos requeridos por cada ruta
   const requiredFields = {
     '/auth/register': ['email', 'nickname', 'password', 'sexo', 'name'],
@@ -14,9 +15,9 @@ export const validateRequiredData = (req: Request, res: Response, next: NextFunc
   const isRequired = requiredFieldsArray.find(field => !req.body[field])
 
   if (isRequired)
-    return res.status(400).json({
+    res.send({
       status: 'error',
-      message: `The field '${isRequired}' is required.`,
+      message: `The field ${isRequired} is required.`,
     })
   else next()
 
