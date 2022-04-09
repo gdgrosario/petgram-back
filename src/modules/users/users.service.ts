405d381da0d80e1967063a64c654e740147046da
Oliver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2022-04-08 23:42:59 -0500

    add: changes for service user

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,9 +2,10 @@ import { BadRequestException, Injectable, NotFoundException } from "@nestjs/comm
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 
-import { CreateUserDto } from "./dtos/user.dtos";
+import { CreateUserDto } from './dtos/user.dtos';
 import { User } from "./entities/user.entity";
 import { Hash } from "../../utils/Hash";
+import { validateRequiredData } from '../../helpers/validateRequiredData';
 
 @Injectable()
 export class UsersService {
@@ -28,26 +29,37 @@ export class UsersService {
   }
 
   async create(data: CreateUserDto) {
-    const { email, nickname, password } = data;
-
-   const userFindWithEmail = await this.userModel.findOne({ email });
-
-    if (userFindWithEmail) {
-      throw new BadRequestException(
-        `The user with the email: '${email}' already exists.`
-      );
-    }
-
+    const { email, nickname, password, sexo } = data;
+     
+    const userFindWithEmail = await this.userModel.findOne({ email });
     const userFindWithNickname = await this.userModel.findOne({ nickname }); 
 
-    if (userFindWithNickname) {
-      throw new BadRequestException(
-        `The user with the nickname: '${nickname}' already exists.`
-      );
-    }
+    if (userFindWithEmail)
+      return new BadRequestException(
+        `The user with the email: '${email}' already exists.`
+      ).getResponse();
 
+    else if(!email) 
+      return new BadRequestException(`The field 'email' is required.`).getResponse();
+    
+    if (userFindWithNickname) {
+      return new BadRequestException(
+        `The user with the nickname: '${nickname}' already exists.`
+        ).getResponse();
+      }
+    else if(!nickname)
+      return new BadRequestException(`The field 'nickname' is required.`).getResponse();
+      
+    if(!password)
+      return new BadRequestException(`The field 'password' is required.`).getResponse();
+    
+    if(!sexo)
+      return new BadRequestException(`The field 'sexo' is required.`).getResponse();
+
+      
     const user = new this.userModel(data);
     user.password = Hash.make(password);
     return await user.save();
   }
 }
+
