0265c60539b71ff1180c8710a27427ba193c358b
Стив <kenethra.74@gmail.com>
2022-06-02 20:19:35 -0600

    feat: validation do not follow more than once

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,76 +1,81 @@
 import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { isValidObjectId, Model, Types } from "mongoose";
+import { isValidObjectId, Model } from "mongoose";
 import { Hash } from "../../utils/Hash";
 import { CreateUserDto, UpdateUserDto } from "./dtos/user.dtos";
 import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel("Users") private readonly userModel: Model<User>) {}
+	constructor(@InjectModel("Users") private readonly userModel: Model<User>) { }
 
-  async findAll(): Promise<User[]> {
-    const users = await this.userModel.find();
-    return users;
-  }
+	async findAll(): Promise<User[]> {
+		const users = await this.userModel.find();
+		return users;
+	}
 
-  async findOne(id: string): Promise<User | undefined> {
-    if (isValidObjectId(id)) {
-      const user = await this.userModel.findById(id);
-      if (!user) {
-        throw new NotFoundException(`The user with the ID: '${id}' was not found.`);
-      }
-      return user;
-    }
-    throw new BadRequestException("id is invalid");
-  }
+	async findOne(id: string): Promise<User | undefined> {
+		if (isValidObjectId(id)) {
+			const user = await this.userModel.findById(id);
+			if (!user) {
+				throw new NotFoundException(`The user with the ID: '${id}' was not found.`);
+			}
+			return user;
+		}
+		throw new BadRequestException("id is invalid");
+	}
 
-  async findByEmail(email: string): Promise<User> {
-    return await this.userModel.findOne({ email });
-  }
+	async findByEmail(email: string): Promise<User> {
+		return await this.userModel.findOne({ email });
+	}
 
-  async create(data: CreateUserDto): Promise<User> {
-    const { email, nickname, password } = data;
+	async create(data: CreateUserDto): Promise<User> {
+		const { email, nickname, password } = data;
 
-    const userFindWithEmailAndNickname = await this.userModel.findOne({
-      $or: [{ email }, { nickname }]
-    });
+		const userFindWithEmailAndNickname = await this.userModel.findOne({
+			$or: [{ email }, { nickname }]
+		});
 
-    if (userFindWithEmailAndNickname) {
-      throw new BadRequestException("The email or nickname already exists.");
-    }
+		if (userFindWithEmailAndNickname) {
+			throw new BadRequestException("The email or nickname already exists.");
+		}
 
-    const user = new this.userModel(data);
-    user.password = Hash.make(password);
-    return await user.save();
-  }
+		const user = new this.userModel(data);
+		user.password = Hash.make(password);
+		return await user.save();
+	}
 
-  async updateProfile(id: string, data: UpdateUserDto): Promise<User> {
-    const user = await this.userModel.findByIdAndUpdate(id, data, { new: true });
+	async updateProfile(id: string, data: UpdateUserDto): Promise<User> {
+		const user = await this.userModel.findByIdAndUpdate(id, data, { new: true });
 
-    return user;
-  }
+		return user;
+	}
 
-  async findOneByUserName(userName: string): Promise<User> {
-    const user = await this.userModel.findOne({ nickname: userName });
-    return user;
-  }
+	async findOneByUserName(userName: string): Promise<User> {
+		const user = await this.userModel.findOne({ nickname: userName });
+		return user;
+	}
 
-  async recoverPassword(id: string, password: string): Promise<{ message: string }> {
-    const user = await this.userModel.findById(id);
-    user.password = Hash.make(password);
-    await user.save();
-    return { message: "Updated successfully" };
-  }
+	async recoverPassword(id: string, password: string): Promise<{ message: string }> {
+		const user = await this.userModel.findById(id);
+		user.password = Hash.make(password);
+		await user.save();
+		return { message: "Updated successfully" };
+	}
 
-  async follow(id: string, idFollow: string): Promise<{ message: string }> {
-    const user = await this.userModel.findById(id);
-    const follower = await this.userModel.findById(idFollow);
+	async follow(id: string, idFollow: string): Promise<{ message: string }> {
+		const userFollower = await this.userModel.find({ followers: { "$all": [idFollow] }, _id: id });
+		if (userFollower.length > 0) {
+			throw new BadRequestException("you already follow this user");
+		} else {
+			const user = await this.userModel.findById(id);
+			const follower = await this.userModel.findById(idFollow);
 
-    await this.userModel.findByIdAndUpdate(user, {
-      $push: { followers: follower }
-    });
+			await this.userModel.findByIdAndUpdate(user, {
+				$push: { followers: follower }
+			});
+		}
 
-    return { message: "Followed successfully " };
-  }
+		return { message: "Followed successfully " };
+	}
 }
