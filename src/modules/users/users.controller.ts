268f1e223389659e4518a1a9151df041039f31b7
Стив <kenethra.74@gmail.com>
2022-06-05 14:00:04 -0600

    hotfix: format

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,14 +1,14 @@
 import {
-	Body,
-	Controller,
-	Get,
-	HttpCode,
-	HttpStatus,
-	Param,
-	Post,
-	Put,
-	Query,
-	UseGuards
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards
 } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { Auth } from "../auth/decorator/auth.decorator";
@@ -18,68 +18,75 @@ import { UsersService } from "./users.service";
 
 @Controller("users")
 export class UsersController {
-	constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
-	@Get()
-	@HttpCode(HttpStatus.OK)
-	@UseGuards(AuthGuard("jwt"))
-	getAll(@Query("limit") limit = 100, @Query("offset") offset = 0): Promise<User[]> {
-		return this.usersService.findAll();
-	}
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard("jwt"))
+  getAll(@Query("limit") limit = 100, @Query("offset") offset = 0): Promise<User[]> {
+    return this.usersService.findAll();
+  }
 
-	@Get("/profile")
-	@HttpCode(HttpStatus.OK)
-	@UseGuards(AuthGuard("jwt"))
+  @Get("/profile")
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard("jwt"))
+  getProfile(@Auth() { id }: User): Promise<User> {
+    return this.usersService.findOne(id);
+  }
 
-	getProfile(@Auth() { id }: User): Promise<User> {
-		return this.usersService.findOne(id)
-	}
+  @Get(":userId")
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard("jwt"))
+  getOne(@Param("userId") userId: string): Promise<User> {
+    return this.usersService.findOne(userId);
+  }
 
-	@Get(":userId")
-	@HttpCode(HttpStatus.OK)
-	@UseGuards(AuthGuard("jwt"))
-	getOne(@Param("userId") userId: string): Promise<User> {
-		return this.usersService.findOne(userId);
-	}
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard("jwt"))
+  update(@Body() updateUserDTO: UpdateUserDto, @Auth() { id }: User): Promise<User> {
+    return this.usersService.updateProfile(id, updateUserDTO);
+  }
 
-	@Put()
-	@HttpCode(HttpStatus.OK)
-	@UseGuards(AuthGuard("jwt"))
+  @Get("/get-user-name/:userName")
+  @HttpCode(HttpStatus.OK)
+  getForUserName(@Param("userName") userName: string): Promise<User> {
+    return this.usersService.findOneByUserName(userName);
+  }
 
-	update(@Body() updateUserDTO: UpdateUserDto, @Auth() { id }: User): Promise<User> {
-		return this.usersService.updateProfile(id, updateUserDTO);
-	}
+  @Put("/recover-password")
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard("jwt"))
+  recoverPassword(
+    @Body() updateUser: UpdateUserDto,
+    @Auth() { id }: User
+  ): Promise<{ message: string }> {
+    return this.usersService.recoverPassword(id, updateUser.password);
+  }
 
-	@Get("/get-user-name/:userName")
-	@HttpCode(HttpStatus.OK)
-	getForUserName(@Param("userName") userName: string): Promise<User> {
-		return this.usersService.findOneByUserName(userName);
-	}
+  @Post("/follow/:idfollow")
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard("jwt"))
+  follow(
+    @Param("idfollow") idFollow: string,
+    @Auth() { id }: User
+  ): Promise<{ message: string }> {
+    return this.usersService.follow(id, idFollow);
+  }
 
-	@Put("/recover-password")
-	@HttpCode(HttpStatus.OK)
-	@UseGuards(AuthGuard("jwt"))
-	recoverPassword(@Body() updateUser: UpdateUserDto, @Auth() { id }: User): Promise<{ message: string }> {
-		return this.usersService.recoverPassword(id, updateUser.password);
-	}
+  @Put("/unfollow/:idfollow")
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard("jwt"))
+  unFollow(
+    @Param("idfollow") idFollow: string,
+    @Auth() { id }: User
+  ): Promise<{ message: string }> {
+    return this.usersService.unFollow(id, idFollow);
+  }
 
-	@Post("/follow/:idfollow")
-	@HttpCode(HttpStatus.OK)
-	@UseGuards(AuthGuard("jwt"))
-	follow(@Param("idfollow") idFollow: string, @Auth() { id }: User): Promise<{ message: string }> {
-		return this.usersService.follow(id, idFollow);
-	}
-
-	@Put("/unfollow/:idfollow")
-	@HttpCode(HttpStatus.OK)
-	@UseGuards(AuthGuard("jwt"))
-	unFollow(@Param("idfollow") idFollow: string, @Auth() { id }: User): Promise<{ message: string }> {
-		return this.usersService.unFollow(id, idFollow);
-	}
-
-	@Get("get-users-by-nickname/:nickname")
-	@HttpCode(HttpStatus.OK)
-	getUsersByNickname(@Param("nickname") nickname: string): Promise<User[]> {
-		return this.usersService.findUsersByNickname(nickname);
-	}
+  @Get("get-users-by-nickname/:nickname")
+  @HttpCode(HttpStatus.OK)
+  getUsersByNickname(@Param("nickname") nickname: string): Promise<User[]> {
+    return this.usersService.findUsersByNickname(nickname);
+  }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -7,119 +7,116 @@ import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
-	constructor(@InjectModel("Users") private readonly userModel: Model<User>) { }
+  constructor(@InjectModel("Users") private readonly userModel: Model<User>) {}
 
-	async findAll(): Promise<User[]> {
-		const users = await this.userModel.find();
-		return users;
-	}
+  async findAll(): Promise<User[]> {
+    const users = await this.userModel.find();
+    return users;
+  }
 
-	async findOne(id: string): Promise<User | undefined> {
-		if (isValidObjectId(id)) {
-			const user = await this.userModel.findById(id);
-			if (!user) {
-				throw new NotFoundException(`The user with the ID: '${id}' was not found.`);
-			}
-			return user;
-		}
-		throw new BadRequestException("id is invalid");
-	}
+  async findOne(id: string): Promise<User | undefined> {
+    if (isValidObjectId(id)) {
+      const user = await this.userModel.findById(id);
+      if (!user) {
+        throw new NotFoundException(`The user with the ID: '${id}' was not found.`);
+      }
+      return user;
+    }
+    throw new BadRequestException("id is invalid");
+  }
 
-	async findByEmail(email: string): Promise<User> {
-		return await this.userModel.findOne({ email });
-	}
+  async findByEmail(email: string): Promise<User> {
+    return await this.userModel.findOne({ email });
+  }
 
-	async create(data: CreateUserDto): Promise<User> {
-		const { email, nickname, password } = data;
+  async create(data: CreateUserDto): Promise<User> {
+    const { email, nickname, password } = data;
 
-		const userFindWithEmailAndNickname = await this.userModel.findOne({
-			$or: [{ email }, { nickname }]
-		});
+    const userFindWithEmailAndNickname = await this.userModel.findOne({
+      $or: [{ email }, { nickname }]
+    });
 
-		if (userFindWithEmailAndNickname) {
-			throw new BadRequestException("The email or nickname already exists.");
-		}
+    if (userFindWithEmailAndNickname) {
+      throw new BadRequestException("The email or nickname already exists.");
+    }
 
-		const user = new this.userModel(data);
-		user.password = Hash.make(password);
-		return await user.save();
-	}
+    const user = new this.userModel(data);
+    user.password = Hash.make(password);
+    return await user.save();
+  }
 
-	async updateProfile(id: string, data: UpdateUserDto): Promise<User> {
-		const user = await this.userModel.findByIdAndUpdate(id, data, { new: true });
+  async updateProfile(id: string, data: UpdateUserDto): Promise<User> {
+    const user = await this.userModel.findByIdAndUpdate(id, data, { new: true });
 
-		return user;
-	}
+    return user;
+  }
 
-	async findOneByUserName(userName: string): Promise<User> {
-		const user = await this.userModel.findOne({ nickname: userName });
-		return user;
-	}
+  async findOneByUserName(userName: string): Promise<User> {
+    const user = await this.userModel.findOne({ nickname: userName });
+    return user;
+  }
 
-	async recoverPassword(id: string, password: string): Promise<{ message: string }> {
-		const user = await this.userModel.findById(id);
-		user.password = Hash.make(password);
-		await user.save();
-		return { message: "Updated successfully" };
-	}
+  async recoverPassword(id: string, password: string): Promise<{ message: string }> {
+    const user = await this.userModel.findById(id);
+    user.password = Hash.make(password);
+    await user.save();
+    return { message: "Updated successfully" };
+  }
 
-	async findUsersByNickname(nickname: string): Promise<User[]> {
-		const users = await this.userModel.find({ nickname: { $regex: `^${nickname}`, $options: "i" } });
-		return users;
-	}
+  async findUsersByNickname(nickname: string): Promise<User[]> {
+    const users = await this.userModel.find({
+      nickname: { $regex: `^${nickname}`, $options: "i" }
+    });
+    return users;
+  }
 
-	async follow(id: string, idFollow: string): Promise<{ message: string }> {
-		const user = await this.userModel.findById(id);
-		const futureFollower = await this.userModel.findById(idFollow)
-		const FollowInUser = await this.userModel.find(
-			{ _id: id, followeds: { $in: idFollow } }
-		)
+  async follow(id: string, idFollow: string): Promise<{ message: string }> {
+    const user = await this.userModel.findById(id);
+    const futureFollower = await this.userModel.findById(idFollow);
+    const FollowInUser = await this.userModel.find({
+      _id: id,
+      followeds: { $in: idFollow }
+    });
 
-		if (!futureFollower || !user)
-			throw new BadRequestException("User not found")
-		if (id === idFollow)
-			throw new BadRequestException("You can't follow yourself")
-		if (FollowInUser.length > 0) {
-			throw new BadRequestException("you already follow this user");
-		} else {
+    if (!futureFollower || !user) throw new BadRequestException("User not found");
+    if (id === idFollow) throw new BadRequestException("You can't follow yourself");
+    if (FollowInUser.length > 0) {
+      throw new BadRequestException("you already follow this user");
+    } else {
+      await this.userModel.findByIdAndUpdate(user, {
+        $inc: { numberOfFollowed: 1 },
+        $push: { followeds: futureFollower }
+      });
 
-			await this.userModel.findByIdAndUpdate(user, {
-				$inc: { numberOfFollowed: 1 },
-				$push: { followeds: futureFollower },
-			})
+      await this.userModel.findByIdAndUpdate(futureFollower, {
+        $push: { followers: user },
+        $inc: { numberOfFollowers: 1 }
+      });
+      return { message: "Followed successfully " };
+    }
+  }
 
-			await this.userModel.findByIdAndUpdate(futureFollower, {
-				$push: { followers: user },
-				$inc: { numberOfFollowers: 1 },
-			})
-			return { message: "Followed successfully " };
-		}
+  async unFollow(id: string, idFollow: string): Promise<{ message: string }> {
+    const user = await this.userModel.findById(id);
+    const followed = await this.userModel.findById(idFollow);
+    const FollowInUser = await this.userModel.find({
+      _id: id,
+      followeds: { $in: idFollow }
+    });
 
-	}
+    if (!followed || !user) throw new BadRequestException("User not found");
+    if (user.numberOfFollowed > 0 && FollowInUser.length > 0) {
+      await this.userModel.findByIdAndUpdate(user, {
+        $inc: { numberOfFollowed: -1 },
+        $pull: { followeds: followed.id }
+      });
 
-	async unFollow(id: string, idFollow: string): Promise<{ message: string }> {
-		const user = await this.userModel.findById(id)
-		const followed = await this.userModel.findById(idFollow)
-		const FollowInUser = await this.userModel.find(
-			{ _id: id, followeds: { $in: idFollow } }
-		)
+      await this.userModel.findByIdAndUpdate(followed, {
+        $pull: { followers: user.id },
+        $inc: { numberOfFollowers: -1 }
+      });
 
-		if (!followed || !user)
-			throw new BadRequestException("User not found")
-		if (user.numberOfFollowed > 0 && FollowInUser.length > 0) {
-			await this.userModel.findByIdAndUpdate(user, {
-				$inc: { numberOfFollowed: -1 },
-				$pull: { followeds: followed.id },
-			})
-
-			await this.userModel.findByIdAndUpdate(followed, {
-				$pull: { followers: user.id },
-				$inc: { numberOfFollowers: -1 },
-			})
-
-			return { message: "Unfollowed successfully " }
-
-		} else throw new BadRequestException("You don't unfollow this user")
-
-	}
+      return { message: "Unfollowed successfully " };
+    } else throw new BadRequestException("You don't unfollow this user");
+  }
 }
