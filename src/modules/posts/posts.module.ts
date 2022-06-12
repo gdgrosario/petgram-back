09c6edbc38cacd85c7c43c32494ee3bbc109b71d
Стив <kenethra.74@gmail.com>
2022-06-12 17:41:11 -0600

    feat: get all and delete

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,19 +1,22 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { CommentSchema } from "../comments/entities/comment.entity";
+import { UserSchema } from "../users/schemas/user.schema";
 import { PostsController } from "./controllers/posts.controller";
 import { PostSchema } from "./schemas/post.schema";
-import { UserSchema } from "../users/schemas/user.schema";
 import { PostsService } from "./services/posts.service";
 
+
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: "Posts", schema: PostSchema },
-      { name: "Users", schema: UserSchema }
-    ])
-  ],
-  controllers: [PostsController],
-  providers: [PostsService],
-  exports: [PostsService]
+	imports: [
+		MongooseModule.forFeature([
+			{ name: "Posts", schema: PostSchema },
+			{ name: "Users", schema: UserSchema },
+			{ name: "Comments", schema: CommentSchema }
+		])
+	],
+	controllers: [PostsController],
+	providers: [PostsService],
+	exports: [PostsService]
 })
-export class PostsModule {}
+export class PostsModule { }
--- src/modules/posts/services/posts.service.ts
+++ src/modules/posts/services/posts.service.ts
@@ -2,22 +2,32 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "src/modules/users/schemas/user.schema";
+import { Comment } from "../../comments/entities/comment.entity";
 import { PostDto } from "../dtos/post.dtos";
 import { Post } from "../schemas/post.schema";
 
 @Injectable()
 export class PostsService {
-  constructor(
-    @InjectModel("Posts") private readonly postModel: Model<Post>,
-    @InjectModel("Users") private readonly userModel: Model<User>
-  ) {}
+	constructor(
+		@InjectModel("Posts") private readonly postModel: Model<Post>,
+		@InjectModel("Users") private readonly userModel: Model<User>,
+		@InjectModel("Comments") private readonly commentModel: Model<Comment>
+	) { }
 
-  async create(data: PostDto, idUser: string): Promise<Post> {
-    const post = new this.postModel(data);
-    const postSave = await post.save();
-    await this.userModel.findByIdAndUpdate(idUser, {
-      $push: { posts: postSave }
-    });
-    return postSave;
+	async findAll(): Promise<Post[]> {
+		return this.postModel.find().populate("comments", "", this.commentModel);
+	}
+
+	async create(data: PostDto, idUser: string): Promise<Post> {
+		const post = new this.postModel(data);
+		const postSave = await post.save();
+		await this.userModel.findByIdAndUpdate(idUser, {
+			$push: { posts: postSave }
+		});
+		return postSave;
+	}
+
+  async delete(id:string): Promise<void> {
+    await this.postModel.findByIdAndRemove(id);
   }
 }
